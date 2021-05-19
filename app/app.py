97cf9151228421ad34125793f9453d224f43b72c
Promiat <59731108+Promiat@users.noreply.github.com>
2021-05-19 19:43:57 +0200

    changment for connexion, that's work fine

--- app/app.py
+++ app/app.py
@@ -1,32 +1,90 @@
-# compose_flask/app.py
+from typing import List, Dict
 from flask import Flask
-# import flask_sqlalchemy
-from sqlalchemy import create_engine
-
-config = {
-    'host' : 'localhost',
-    'port' : '3320',
-    'user' : 'newuser',
-    'password' : 'example',
-    'database' : 'classicmodels',
-}
-
-db_user = config.get('user')
-db_pwd = config.get('password')
-db_host = config.get('user')
-db_port = config.get('host')
-db_name = config.get('database')
+import json
+from sqlalchemy import create_engine, MetaData, Table, Column, Float, Integer, String, ForeignKey
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.dialects.mysql import DATETIME
+import logging
 
 app = Flask(__name__)
 
-def create_app():
-    connection_str = 'mysql+mysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
-    engine = create_engine(connection_str)
-    connection = engine.connect()
+config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'classicmodels'
+    }
 
+db_user = config.get('user')
+db_pwd = config.get('password')
+db_host = config.get('host')
+db_port = config.get('port')
+db_name = config.get('database')
+
+connection_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
+# connect to database
+engine = create_engine(connection_str)
+connection = engine.connect()
+logging.info(' - - - ✅ MySQL Docker Container Python connection ok - - - \n')
+
+metadata = MetaData(bind=engine)
+metadata.reflect(only=['customers','employees','offices', 'orderdetails','orders','payments','productlines','products'])
+
+
+#mapping of metadata
+Base = automap_base(metadata=metadata)
+Base.prepare()
+payments = Base.classes.payments
+
+logging.info('\n - - - ✅ [Tables] Payments | Data Mapping OK - - - \n')
+
+def print_table() -> List:
+    tab=[]
+    logging.info(' - - - ✅ Tables into database - - - \n')
+    for t in metadata.sorted_tables:
+        print("\t\t - {}".format(t.name))
+        tab.append(t.name)
+    return tab 
+
+#create session and perform request 
+from sqlalchemy.orm import sessionmaker
+from datetime import date
+Session = sessionmaker()
+# associate session with our db 
+Session.configure(bind=engine)
+session = Session()
+
+def sample_query() -> List:    
+    #query db
+    q=[]
+    prilogging.info('\n### ✅ All Payments with date > 01 June 2005:')
+    pays = session.query(payments)\
+        .filter_by(paymentDate > date(2005,6,1))\
+        .all()
+    for pay in pays:
+        q.append('{pay.customerNumber} payed {pay.amount} on {pay.paymentDate}')
+    print('')
+    return q 
+def new_query() -> List:
+    q = []
+    payments = session.query(payments)\
+        .filter_by(amount > 100000)\
+         .all()
+    for pay in payments: 
+        q.append('{pay.customerNumber} payed {pay.amount}')     
+    return q   
 @app.route('/')
-def index():
-    return ' - - - MYSQL Database `classicmodels` connection ok - - - '
+def index() -> str:
+    return ' - - - ✅ MySQL Database `classicmodels` connection ok - - - '
+
+@app.route('/tables')
+def tables() -> str:
+    return json.dumps({'Tables ': print_table()})
+
+@app.route('/query')
+def new_query() -> str:
+    return json.dumps({'Sample Query ': new_query()})
 
 
 if __name__ == '__main__':
